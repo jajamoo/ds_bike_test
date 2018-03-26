f3fc099110bb27a08ae70eb44836ec1f3564d541
MoeSymDistro <esanul@symdistro.com>
2018-03-26 13:44:58 -0400

    change datetime parse,

--- bike_workflow/data.py
+++ bike_workflow/data.py
@@ -5,10 +5,16 @@ import pandas as pd
 FREMONT_URL = 'https://data.seattle.gov/api/views/65db-xm6k/rows.csv?accessType=DOWNLOAD'
 
 
-def get_data (file = 'fremont.csv', url = FREMONT_URL, force_dl = False):
+def get_data(file = 'fremont.csv', url = FREMONT_URL, force_dl = False):
     if force_dl or not os.path.exists(file):
         urlretrieve(url, file)
-    data = pd.read_csv('fremont.csv', index_col = 'Date', parse_dates = True)
+    data = pd.read_csv('fremont.csv', index_col = 'Date')
+
+    try:
+        data.index = pd.to_datetime(data.index, format = '%m/%d/%Y %H:%M:%S %p')
+    except TypeError:
+        data.index = pd.to_datetime(data.index)
+
     data.columns = ['West', 'East']
     data['Total'] = data['West'] + data['East']
     return data
