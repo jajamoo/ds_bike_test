97a56ef5372ef7c861829f75628707effe8aafb6
MoeSymDistro <esanul@symdistro.com>
2018-03-26 13:34:30 -0400

    unit testing and makefile

---
+++ bike_workflow/tests/test_data.py
@@ -0,0 +1,10 @@
+from bike_workflow.data import get_data
+import pandas as pd
+
+
+def test_data():
+    data = get_data()
+
+    assert all(data.columns == ['West', 'East', 'Total'])
+    assert isinstance(data.index, pd.DatetimeIndex)
+
